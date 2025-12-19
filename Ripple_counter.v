236a39443d905e8ff07b61b4e48491819b1e703a
Santhiparthiban <pshanthi593@gmail.com>
2025-12-19 12:06:34 +0530

    Add files via upload

---
+++ Ripple_counter.v
@@ -0,0 +1,20 @@
+module Ripple_counter (
+    input  wire clk,       
+    input  wire rst,       
+    output reg  [2:0] q   
+);
+
+initial begin
+     q <= 3'b000;
+	 end
+
+always @(posedge clk) 
+begin
+q <= 3'b000;
+    if (rst) 
+        q <= 3'b000;       
+    else
+        q <= q + 1;        
+end
+endmodule
+
